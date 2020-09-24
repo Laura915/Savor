9e5a1e72bca45853efc45a87d20a4d23badd61b2
Laura Hernandez <34899738+Laura915@users.noreply.github.com>
2020-09-24 15:51:25 -0400

    Update fork to base master (#1)
    * added in call function for itunes API that pulls name and genre and image and song name and song url.
    
    * added styling to html and made ids classes to connect js and html
    
    * fixed the itunes api. Also changed the songtitle and artistname variables to classes.
    
    * Update README.md
    
    * Delete index.html
    
    * included commit without any html.
    
    * html update
    
    * got the functionality of the artistName and songTitle being searched. Also added artist image and link for bio.
    
    * Added Clear function
    
    * changed some html to display and or hide. Added some CSS styling.
    
    * added HTML elements to artist card and info from json object in JS.Added RegExp to format lyrics in poem form
    
    * removed unnecessary files and folder.
    
    * Updated Background
    
    * added dynamic change to artist information card, changed button and container colors to match theme, and included logic to stop a user from entering an empty input.
    
    * adjusted the structure of the onClick so that the boxes will not populate when the button is clicked.
    
    * removed the additional images.
    
    * added website to read me
    
    * Updated read me to be more readable.
    
    * fixed background image bug
    
    * cleaned up index.html code
    
    * Cleaned up code by taking out commented out code
    
    Co-authored-by: Casey VanAlstyne <caseywvanalstyne@gmail.com>
    Co-authored-by: Mónica Polanco <mpolan16@g.holycross.edu>
    Co-authored-by: Casey VanAlstyne <61564549+CaseyVanAlstyne@users.noreply.github.com>
    Co-authored-by: Mpolan16 <60660512+Mpolan16@users.noreply.github.com>
    Co-authored-by: Matt <51001542+itsTadae@users.noreply.github.com>
    Co-authored-by: Matthew Henry <tadaemusic@gmail.com>

--- script.js
+++ script.js
@@ -1,29 +1,64 @@
-var searchButton = $("#searchLyricsButton");
-var outputLyrics = $("#output");
+$(document).ready(function () {
+  var searchButton = $("#searchLyricsButton");
+  var outputLyrics = $("#outputSearch");
 
-// onclick function for main search button
-$("#searchLyricsButton").on("click", function () {
-    var artistName = $("#artistName").val();
-    var songTitle = $("#songTitle").val();
-    event.preventDefault();
-    findLyrics(artistName, songTitle);
-})
+  // onclick function for main search button
+  $("#searchLyricsButton").on("click", function () {
+    var artistName = $(".artistName").val();
+    var songTitle = $(".songTitle").val();
+    if (artistName === "" && songTitle === "") {
+      return;
+    } else {
+      $("#outputSearch").empty();
+      $("#lyricContainer").removeClass("hide").addClass("show");
+      $("#artistContainer").removeClass("hide").addClass("show");
+      event.preventDefault();
+      findLyrics(artistName, songTitle);
+      callItunesAPI(artistName, songTitle);
+      $("#icon_prefix").val("");
+      $("#icon_telephone").val("");
+    }
+  });
 
-// function to pull information from lyrics.ovh api and display on screen
-function findLyrics(artistName, songTitle, outputLyrics) {
+  // function to pull information from lyrics.ovh api and display on screen
+  function findLyrics(artistName, songTitle) {
     var queryURL = "https://api.lyrics.ovh/v1/" + artistName + "/" + songTitle;
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function (response) {
-            // console.log("current stuff", response.lyrics);
-            var pullLyrics = response.lyrics;
-            $("#output").prepend(pullLyrics);
-        })
-}
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      //console.log("current stuff", response.lyrics);
+      var pullLyrics = response.lyrics.replace(new RegExp("\n", "g"), "<br>");
+      $("#outputSearch").prepend(pullLyrics);
+    });
+  }
 
-// $.get("https://api.lyrics.ovh/v1/" + document.getElementById("artistName").value + "/" + document.getElementById("songTitle").value,
-            //     function (data) {
-            //         document.getElementById("output").innerHTML = data.lyrics.replace(new RegExp("\n", "g"), "<br>")
-            //     })
+  function callItunesAPI(artistName, songTitle) {
+    var queryURL =
+      "https://cors-anywhere.herokuapp.com/" + "https://itunes.apple.com/search?term=" + artistName + "/" + songTitle + "&type=songs&limit=1";
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      var result = JSON.parse(response).results;
+      artist = {
+        name: result[0].artistName,
+        genre: result[0].primaryGenreName,
+        imageURL: result[0].artworkUrl100,
+        artistViewUrl: result[0].artistViewUrl,
+        songName: result[0].trackName,
+        // songURL: result[0].previewUrl
+      };
+      // console.log("current log", result)
+      // console.log("this is 100", result[0].artworkUrl100)
+      // console.log("this is artist view url", result[0].artistViewUrl)
+      $("#artistInfo").attr("href", result[0].artistViewUrl);
+      $("#imageOutput").attr("src", result[0].artworkUrl100);
+      $("#imageOutput").attr("alt", result[0].artistName);
+      $(".artistNameInfo").text("Artist Name: " + artist.name);
+      $(".genreNameInfo").text("Genre: " + artist.genre);
+      $(".trackNameInfo").text("Song: " + artist.songName);
+      $("#artistInfo").text("Click here for " + artist.name + "'s information");
+    });
+  }
+});
